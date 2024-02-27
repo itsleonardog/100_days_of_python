8bbcb35c9f99131672fbbb7115604a2e0dfc806b
Leonardo Genzolini <97109108+itsleonardog@users.noreply.github.com>
2024-02-27 16:17:11 +0100

    update UI

--- 028-pomodoro-timer/main.py
+++ 028-pomodoro-timer/main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-# ---------------------------- CONSTANTS ------------------------------- #
+
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
@@ -20,10 +20,21 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
+title_label.grid(column=1, row=0)
+
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_image = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_image)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
+
+start_button = Button(text="Start", highlightbackground=YELLOW)
+start_button.grid(column=0, row=2)
+reset_button = Button(text="Reset", highlightbackground=YELLOW)
+reset_button.grid(column=2, row=2)
+
+check_marks = Label(text="✔️", fg=GREEN, bg=YELLOW)
+check_marks.grid(column=1, row=3)
 
 window.mainloop()
