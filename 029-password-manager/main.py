e0ee4a7b934eca19b62c7b267830a12a83212478
Leonardo Genzolini <97109108+itsleonardog@users.noreply.github.com>
2024-03-06 19:00:11 +0100

    focus on the first entry and email insert

--- 029-password-manager/main.py
+++ 029-password-manager/main.py
@@ -24,8 +24,10 @@ password_label.grid(row=3, column=0)
 # entries
 website_entry = Entry(width=36)
 website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus()
 email_entry = Entry(width=36)
 email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, "leonardogenzolini@gmail.com")
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
