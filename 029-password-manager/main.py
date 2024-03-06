e7ed4366c3f1bf510c72c4f087aceabda9cdc540
Leonardo Genzolini <97109108+itsleonardog@users.noreply.github.com>
2024-03-06 19:18:38 +0100

    save password to data.txt file

--- 029-password-manager/main.py
+++ 029-password-manager/main.py
@@ -1,7 +1,18 @@
 from tkinter import *
 # PASSWORD GENERATOR
 
+
 # SAVE PASSWORD
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    if website and email and password:
+        with open("data.txt", "a") as data_file:
+            data_file.write(f"{website} | {email} | {password}\n")
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
 
 #  UI SETUP
 window = Tk()
@@ -34,7 +45,7 @@ password_entry.grid(row=3, column=1)
 # buttons
 generate_password_button = Button(text="Generate Password")
 generate_password_button.grid(row=3, column=2)
-add_button = Button(text="Add", width=32)
+add_button = Button(text="Add", width=32, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
