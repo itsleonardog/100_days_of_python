28ee8b77f4123347e9c16a06c35928277110b7b1
Leonardo Genzolini <97109108+itsleonardog@users.noreply.github.com>
2024-03-08 14:19:07 +0100

    handling exceptions

--- 029-password-manager/main.py
+++ 029-password-manager/main.py
@@ -38,13 +38,19 @@ def save():
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please fill in both the website and password fields.")
     else:
-        with open("data.json", "r") as data_file:
-            data = json.load(data_file)
+        try:
+            with open("data.json", "r") as data_file:
+                data = json.load(data_file)
+                data.update(new_data)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
             data.update(new_data)
 
-        with open("data.json", "w"):
-            json.dump(data, data_file, indent=4)
-
+            with open("data.json", "w"):
+                json.dump(data, data_file, indent=4)
+        finally:
             website_entry.delete(0, END)
             password_entry.delete(0, END)
 
