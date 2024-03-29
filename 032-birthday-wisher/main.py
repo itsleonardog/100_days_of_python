47a96b5bfe03f0116244da602f8b5055ab3c3284
Leonardo Genzolini <97109108+itsleonardog@users.noreply.github.com>
2024-03-29 12:09:35 +0100

    app init, imports, and dictionary creation

---
+++ 032-birthday-wisher/main.py
@@ -0,0 +1,29 @@
+import pandas
+from datetime import datetime
+
+dt = datetime.today()
+today_day = dt.day
+today_month = dt.month
+today = (today_month, today_day)
+
+data = pandas.read_csv("birthdays.csv")
+birthdays_dict = {
+    (row['month'], row['day']): row[['name', 'email', 'year', 'month', 'day']].tolist()
+    for _, row in data.iterrows()
+}
+
+if (today_month, today_day) in birthdays_dict:
+
+# 3. If there is a match, pick a random letter (letter_1.txt/letter_2.txt/letter_3.txt) from letter_templates and replace the [NAME] with the person's actual name from birthdays.csv
+# HINT 1: Think about the relative file path to open each letter. 
+# HINT 2: Use the random module to get a number between 1-3 to pick a randome letter.
+# HINT 3: Use the replace() method to replace [NAME] with the actual name. https://www.w3schools.com/python/ref_string_replace.asp
+
+# 4. Send the letter generated in step 3 to that person's email address.
+# HINT 1: Gmail(smtp.gmail.com), Yahoo(smtp.mail.yahoo.com), Hotmail(smtp.live.com), Outlook(smtp-mail.outlook.com)
+# HINT 2: Remember to call .starttls()
+# HINT 3: Remember to login to your email service with email/password. Make sure your security setting is set to allow less secure apps.
+# HINT 4: The message should have the Subject: Happy Birthday then after \n\n The Message Body.
+
+
+
